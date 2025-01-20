2defda895dc05f88d255a683f8b2f3942aab70f0
way <k8652090202@gmail.com>
2025-01-20 14:07:29 +0800

    feat : 系統訊息顯示

--- Core/Character.php
+++ Core/Character.php
@@ -7,7 +7,7 @@ use Core\Database;
 // [ life => 生命值, magic => 魔力值, attack => 物理攻擊, mag => 魔法攻擊, defense => 物理防禦, mddf=>魔法防禦, speed => 速度, lucky => 幸運值]
 class Character{
     protected $level;# 角色等級
-    protected $name; # 名稱
+    public $name; # 名稱
     protected $role; #角色   // Warrior（戰士），Mage（法師），Priest（牧師），Rogue（盜賊），Paladin（騎士） Monster(怪物)
     public $attribute; # 存放角色屬性
 
---
+++ Core/Message.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Core;
+
+require_once('Interfaces/ObserverInterface.php');
+
+use Core\Interfaces\Subject;
+
+class Message implements Subject{
+    private $observers = [];
+    private $message;
+
+    public function attach($observer){
+        echo "歡迎挑戰者 $observer->name 進入.";
+        $this->observers[] = $observer;
+    }
+    public function detach($observer){
+        $index = array_search($observer,$observers);
+        // if($index!== false){
+        unset($observers[$index]);
+        // }
+    }
+    public function notify(){
+        foreach($this->observers as $observer){
+            $observer->update($this->message);
+        }
+    }
+
+    public function setmessage($message){
+        $this->message = $message;
+        $this->notify();
+    }
+}
