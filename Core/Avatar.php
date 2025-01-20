0506661d33691b1ffc9018373ada4161eb53bde9
way <k8652090202@gmail.com>
2025-01-20 14:07:29 +0800

    feat : 系統訊息顯示

--- Core/Avatar.php
+++ Core/Avatar.php
@@ -1,29 +1,34 @@
 <?php
 namespace Core;
 
-use Core\Character;
+require_once('Interfaces/ObserverInterface.php');
 
-class Avatar extends Character{
+use Core\Character;
+use Core\Interfaces\OBserver;
+
+class Avatar extends Character implements Observer{ //implements Observer{
     protected $money;
     protected $death_time;
 
     function __construct($username,$role){
         $this->init($username,$role);
-        $db = $this->DBconnect();
-        $attribute_json = json_encode($this->attribute);
-        $db->query("insert into player (name,level,role,attribute,money) values ('{$this->name}',{$this->level},'{$this->role}','{$attribute_json}',{$this->money});");
-        
+        // $db = $this->DBconnect();
+        // $attribute_json = json_encode($this->attribute);
+        // $db->query("insert into player (name,level,role,attribute,money) values ('{$this->name}',{$this->level},'{$this->role}','{$attribute_json}',{$this->money});");
     }
 
     public function init($username,$role){
         $this->name = $username;
-        $this->level = 12;
+        $this->level = 1;
         $this->role = $role;
         $this->money = 50;
         $this->death_time = 0;
         $point = calculate_total_point($this->role,$this->level);
         $this->attribute = response_points($this->init[$role],$point,$role);
-
+    }
+    
+    public function update($message){
+        echo $message;
     }
 
     #todo : 暫時先把復活的邏輯寫下，後續要再依照情況調整
