92d43a7489b604c529d62543e1ba45adf43a09d8
way <k8652090202@gmail.com>
2025-01-20 15:45:42 +0800

    refactor : 把屬性比例設定改放到config檔案裡面

--- Core/Monster.php
+++ Core/Monster.php
@@ -6,26 +6,27 @@ use Core\Character;
 class Monster extends Character{
     protected $reward = array();
 
-    # $player_level
     # * 這邊先以建構子跑新建的流程之後會變成呼叫的函式
-    # todo Avator那邊要在確認是否已經註冊＋要把拿player id的拆出去變成獨立函式，moster要在加id欄位
+    # todo 要把拿player id的拆出去變成獨立函式
     function __construct($player_name){
-        $this->level = 20;
+        $this->level = 1;
         $this->role = 'Monster';
-        $db = $this->DBconnect();
-        $player =  $db->query("select * from player where name='$player_name';")->find_or_fail();
-        $this->generate($this->level,$this->role,$player['level']);
-        $attribute_json = json_encode($this->attribute);
-        $reward_json = json_encode($this->reward);
-        if(!empty($player)){
-            $db->query("insert into monster (name,level,attribute,player_id,reward) values ('{$this->name}','{$this->level}','{$attribute_json}',{$player['id']},'{$reward_json}');");
-        }
+        // $db = $this->DBconnect();
+        // $player =  $db->query("select * from player where name='$player_name';")->find_or_fail();
+        // $this->generate($this->level,$this->role,$player['level']);
+        // $attribute_json = json_encode($this->attribute);
+        // $reward_json = json_encode($this->reward);
+        // if(!empty($player)){
+        //     $db->query("insert into monster (name,level,attribute,player_id,reward) values ('{$this->name}','{$this->level}','{$attribute_json}',{$player['id']},'{$reward_json}');");
+        // }
     }
     
     public function generate($level,$role,$player_lv){
+        $setting = get_setting();
+        $attribute_percent = $setting['attribute_percent'];
         $this->name = $this->rand_name();
         $point = calculate_total_point($this->role,$this->level);
-        $this->attribute = response_points($this->init[$role],$point,$role);
+        $this->attribute = response_points($attribute_percent[$role],$point,$role);
         // 經驗值 = B × ( 1 + M/10 ) x ( 1 + lv-player/lv-monster)
         // 金錢獎勵=G×(1+ lv-monster/20)
         $this->reward['money'] = ceil(200*(1+$level/20));
