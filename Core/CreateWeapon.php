24d548e54680c94f9f3c58d643abca8316e5f423
way <k8652090202@gmail.com>
2025-01-23 12:00:41 +0800

    feat : 裝備系統初步架構

---
+++ Core/CreateWeapon.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Core;
+
+class CreateAx{
+    public function __construct()
+    {
+        echo "Ax";   
+    }
+}
+class CreateWand{
+    public function __construct()
+    {
+        echo "Wand";   
+    }
+}
+class CreateDagger{
+    public function __construct()
+    {
+        echo "Dagger";   
+    }
+}
+
