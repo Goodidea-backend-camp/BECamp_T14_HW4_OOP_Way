e12c720a89c7efb559542ced72e68fb32cda0abf
way <k8652090202@gmail.com>
2025-01-23 12:00:41 +0800

    feat : 裝備系統初步架構

---
+++ Core/Weapon.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Core;
+require_once('Interfaces/EquipmentInterface.php');
+use Core\Interfaces\Equipment;
+use Core\CreateAx;
+// use Core\CreateWand;
+// use Core\CreateDagger;
+
+class Weapon implements Equipment{
+    public $user;#用來存放class avatar 或是 monster的資料
+    
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    public static function create($user){
+        if($user->get('role') === 'Mage'){
+            return new CreateWand();
+        }elseif($user->get('role') === 'Warrior'){
+            return new CreateAx();
+        }elseif($user->get('role') === 'Rogue'){
+            return new CreateDagger();
+        }
+    }
+    public function belongTo($user){}
+
+    public function changeOwner($newOwner){}
+
+    public function limitCheck($user){}
+
+    public function effect(){}
+}
