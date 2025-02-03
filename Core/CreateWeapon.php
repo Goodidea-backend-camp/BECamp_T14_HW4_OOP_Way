d370889fe51c54bd6960a052590121ed97161641
way <k8652090202@gmail.com>
2025-02-03 14:43:26 +0800

    feat : 武器生成攻擊、金錢價值、特殊效果

--- Core/CreateWeapon.php
+++ Core/CreateWeapon.php
@@ -2,22 +2,28 @@
 
 namespace Core;
 
-class CreateAx{
-    public function __construct()
-    {
-        echo "Ax";   
+use Core\Creator;
+
+# 攻擊力 = (基礎值 + 等級修正:2*等級) × 稀有度修正
+# 價格 = 攻擊力 × 稀有度加成 × 0.8~1.2
+# 耐久度 = (基礎值 × 類型修正) + 隨機波動 <- 先不實作
+
+class CreateAx extends Creator{
+    public function __construct($user)
+    {   
+        [$rarity,$moneyWeight,$attackWeight,$attack,$money,$specialEffects] = $this->weapon($user,'Ax');
     }
 }
-class CreateWand{
-    public function __construct()
+class CreateWand extends Creator{
+    public function __construct($user)
     {
-        echo "Wand";   
+        [$rarity,$moneyWeight,$attackWeight,$attack,$money,$specialEffects] = $this->weapon($user,'Wand');
     }
 }
-class CreateDagger{
-    public function __construct()
+class CreateDagger extends Creator{
+    public function __construct($user)
     {
-        echo "Dagger";   
+        [$rarity,$moneyWeight,$attackWeight,$attack,$money,$specialEffects] = $this->weapon($user,'Dagger');
     }
 }
 
