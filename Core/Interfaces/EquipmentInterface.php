51cdebd387d95954b155a68ed0973e8f928ff30f
way <k8652090202@gmail.com>
2025-02-04 16:07:14 +0800

    feat : 產生武器名字、武器屬於哪個玩家和寫入資料庫

--- Core/Interfaces/EquipmentInterface.php
+++ Core/Interfaces/EquipmentInterface.php
@@ -4,7 +4,7 @@ namespace Core\Interfaces;
 
 interface Equipment {
     public static function create($user);
-    public function belongTo($user);
+    public function belongTo($weapon);
     public function changeOwner($newOwner);
     public function limitCheck($user);
     public function effect();
