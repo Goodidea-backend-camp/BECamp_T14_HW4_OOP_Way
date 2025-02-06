72155d23ed74b1a322d629ed292804d6c45eec25
way <k8652090202@gmail.com>
2025-02-06 16:25:00 +0800

    refactor : 拆掉$weapon外面那一層(result)，讓回傳值變成陣列

--- Core/Weapon.php
+++ Core/Weapon.php
@@ -24,23 +24,24 @@ class Weapon implements Equipment{
         }elseif($user->get('role') === 'Rogue'){
             $weapon =  new CreateDagger($user,$level);
         }
+        $weapon = (array)$weapon;
 
-        return $weapon;
+        return $weapon['result'];
     }
     
     public function belongTo($weapon){
         $weapon = (array)$weapon; 
-        $value = "'".$weapon['result']['name']."','".$weapon['result']['type']."','".$weapon['result']['rarity']."','".$weapon['result']['attack']."','".$weapon['result']['money']."','".$weapon['result']['level_requirement']."','".$weapon['result']['charactor_requirement']."'";
+        $value = "'".$weapon['name']."','".$weapon['type']."','".$weapon['rarity']."','".$weapon['attack']."','".$weapon['money']."','".$weapon['level_requirement']."','".$weapon['charactor_requirement']."'";
         $dbConfig = getSetting("Database");
         $db = new Database($dbConfig);
         $db->query("INSERT INTO weapon (name, type, rarity, attack, money, level_requirement, charactor_requirement) VALUES ($value)");
         
-        $weaponID = $db->query("SELECT id FROM weapon WHERE name='".$weapon['result']['name']."'")->find_or_fail('one');
+        $weaponID = $db->query("SELECT id FROM weapon WHERE name='".$weapon['name']."'")->find_or_fail('one');
         $userID = $db->query("SELECT id FROM player WHERE name='".$this->user->get('name')."'")->find_or_fail('one');
         $db->query("INSERT INTO player_weapon_relationships (player_id, weapon_id) VALUES (".$userID['id'].",".$weaponID['id'].")");
         
-        if($weapon['result']['specialEffects'] !== NULL){
-            foreach($weapon['result']['specialEffects'] as $specialEffects){
+        if($weapon['specialEffects'] !== NULL){
+            foreach($weapon['specialEffects'] as $specialEffects){
                 $specialEffect = $specialEffects['id'];
                 $db->query("INSERT INTO weapon_special_relationships (weapon_id, special_id) VALUES (".$weaponID['id'].",$specialEffect)");
             }
