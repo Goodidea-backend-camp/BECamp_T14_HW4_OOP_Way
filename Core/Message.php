76270c86fb1f7d86f3d876bdb2a3263aa5bbef17
way <k8652090202@gmail.com>
2025-02-06 13:12:51 +0800

    feat : 商店顯示商品

--- Core/Message.php
+++ Core/Message.php
@@ -30,4 +30,33 @@ class Message implements Subject{
         $this->message = $message;
         $this->notify();
     }
+
+    public function printTable(array $header, array $items){
+        $columnWidths = array(); #需要的字串大小
+        $option = 0; #選項
+        $format= '%-'.strlen("option")."s | "; #文字格式
+        
+        foreach($header as $key=>$title){
+            $columnWidths[$key] = max(strlen($title),strlen($items[0][$key]))+4;
+            $format .= "%-".($columnWidths[$key])."s | ";
+        }
+        $header = array_merge(["option" => "Option"], $header);
+        $format .= "\n";
+
+        # 印出標頭
+        printf($format, ...array_values($header));
+        echo str_repeat("-", array_sum($columnWidths) + (count($header) * 4)) . "\n";
+        unset($header['option']); # 避免下面去用option去找items的東西
+
+        foreach ($items as $item) {
+            $row = [];
+            $row[] = $option;
+            foreach ($header as $key => $title) {
+                $row[] = $item[$key] ?? "NULL";
+            }
+            printf($format, ...$row);
+            $option += 1;
+        }
+
+    }
 }
---
+++ Core/Traits/StoreTrait.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Core\Traits;
+
+use Core\Database;
+use Core\Weapon;
+use Core\Message;
+
+trait Store{
+    public function dbConnect(){
+        $dbConfig = getSetting("Database");
+        return new Database($dbConfig);
+    }
+    public function provide($player){
+        $message = new Message();
+        $db = $this->dbConnect();
+        $playerLevel = $player->get('level');
+        $playerRole = $player->get('role');
+        $result = $db->query('SELECT weapon_id FROM player_weapon_relationships WHERE player_id = 0')->find_or_fail('all');
+        $result = "(".implode(',', array_column($result, 'weapon_id')).")";
+        $products = $db->query("SELECT id,name,type,rarity,attack,money,level_requirement,charactor_requirement FROM weapon WHERE id IN $result AND (level_requirement BETWEEN $playerLevel-1 AND $playerLevel+5) AND charactor_requirement = '$playerRole'")->find_or_fail('all');
+        $headers = [
+            "name" => "Item Name",
+            "attack" => "Attack",
+            "money" => "Money",
+            "level_requirement" => "Level Req",
+            "charactor_requirement" => "Char Req"
+        ];
+
+        if($products !== ''){
+            $generateProduct = $this->generateProduct($player,3-count($products));
+            array_push($products,$generateProduct[0]);
+        }else{
+            $products = $this->generateProduct($player,3);
+        }
+
+        usort($products, function ($a, $b) { return strlen($b['name']) - strlen($a['name']); }); # 把名字最長的放在最陣列前面方便後面的排版
+        $message->printTable($headers,$products);
+    }
+
+    public function sell(){}
+    public function buy(){}
+
+    public function generateProduct($player,$time){
+        $products = array();
+        $weapon = new Weapon($player);
+        for($i=0;$i<$time;$i++){
+            $generateWeapon = (array)$weapon->create($player,$player->get('level')+rand(0,5));
+            $generateWeapon = $generateWeapon['result'];
+            array_push($products, $generateWeapon);
+        }
+        return $products;
+    }
+}
