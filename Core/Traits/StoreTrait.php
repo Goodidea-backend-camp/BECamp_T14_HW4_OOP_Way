6390a62358db0eda67a2479ac6e0cae77d67a2d3
way <k8652090202@gmail.com>
2025-02-07 17:04:47 +0800

    fix ：只能產生一組武器問題

--- Core/Traits/StoreTrait.php
+++ Core/Traits/StoreTrait.php
@@ -11,6 +11,7 @@ trait Store{
         $dbConfig = getSetting("Database");
         return new Database($dbConfig);
     }
+
     public function provide($player){
         $message = new Message();
         $db = $this->dbConnect();
@@ -19,6 +20,7 @@ trait Store{
         $result = $db->query('SELECT weapon_id FROM player_weapon_relationships WHERE player_id = 0')->find_or_fail('all');
         $result = "(".implode(',', array_column($result, 'weapon_id')).")";
         $products = $db->query("SELECT id,name,type,rarity,attack,money,level_requirement,charactor_requirement FROM weapon WHERE id IN $result AND (level_requirement BETWEEN $playerLevel-1 AND $playerLevel+5) AND charactor_requirement = '$playerRole'")->find_or_fail('all');
+        $products = $products === "" ? [] : $products;
         $headers = [
             "name" => "Item Name",
             "attack" => "Attack",
@@ -27,13 +29,12 @@ trait Store{
             "charactor_requirement" => "Char Req"
         ];
 
-        if($products !== ''){
-            $generateProduct = $this->generateProduct($player,3-count($products));
-            array_push($products,$generateProduct[0]);
-        }else{
-            $products = $this->generateProduct($player,3);
+        $generateProduct = $this->generateProduct($player,3-count($products));
+        $time = 3-count($products);
+        for($i=0; $i<$time; ++$i){
+            array_push($products,$generateProduct[$i]);
         }
-
+        
         usort($products, function ($a, $b) { return strlen($b['name']) - strlen($a['name']); }); # 把名字最長的放在最陣列前面方便後面的排版
         $message->printTable($headers,$products);
     }
@@ -46,7 +47,6 @@ trait Store{
         $weapon = new Weapon($player);
         for($i=0;$i<$time;$i++){
             $generateWeapon = (array)$weapon->create($player,$player->get('level')+rand(0,5));
-            $generateWeapon = $generateWeapon['result'];
             array_push($products, $generateWeapon);
         }
         return $products;
