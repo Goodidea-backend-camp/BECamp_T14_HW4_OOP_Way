dd13aa771ca9f76edbfc3059af1786a689eb6d50
way <k8652090202@gmail.com>
2025-02-11 16:46:53 +0800

    feat ：新增角色

--- config.php
+++ config.php
@@ -16,9 +16,10 @@ function getSetting($data){
             # 'life','magic','attack','mag','defense','mddf','speed','lucky' 的百分比
             'Warrior' => [30,5,25,5,20,5,5,5],
             'Mage' => [10,30,5,25,10,10,5,5],
-            #'Priest' => ['life'=>10,'magic'=>10,'attack'=>10,'mag'=>10,'defense'=>10,'mddf'=>10,'speed'=>10,'lucky'=>10],
+            'Priest' => [10,30,5,30,5,10,5,5],
             'Rogue' => [15,10,15,10,10,5,30,5],
-            #'Paladin' => ['life'=>10,'magic'=>10,'attack'=>10,'mag'=>10,'defense'=>10,'mddf'=>10,'speed'=>10,'lucky'=>10],
+            // 'Paladin' => ['life'=>10,'magic'=>10,'attack'=>10,'mag'=>10,'defense'=>10,'mddf'=>10,'speed'=>10,'lucky'=>10],
+            'Paladin' => [30,5,20,5,25,5,5,5],
             'Monster' => [20,30,5,25,5,10,5,0],
         ],
     ];
